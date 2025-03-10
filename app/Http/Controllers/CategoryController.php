c9ae51a2950225f9bf0471e1927ba08709ffd561
Yazky Maulana <yazki.maulana@gmail.com>
2025-03-11 00:10:03 +0700

    feat: feat edit, delete category and make new request files

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -65,15 +66,29 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        DB::transaction(function () use ($request, $category) {
+            $validated = $request->validated();
+
+            if($request->hasFile('icon')){
+                $iconPath = $request->file('icon')->store('icons', 'public');
+                $validated['icon'] = $iconPath;
+            }
+            
+            $validated['slug'] = Str::slug($validated['name']);
+            
+            $category->update($validated);
+
+        });
+
+        return redirect()->route('admin.categories.index');
     }
 
     /**
@@ -81,6 +96,17 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        DB::beginTransaction();
+
+        try {
+            $category->delete();
+            DB::commit();
+
+            return redirect()->route('admin.categories.index');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            
+            return redirect()->route('admin.categories.index')->with('error', 'Terjadinya sebuah error ketika menghapus data category');
+        }
     }
 }
