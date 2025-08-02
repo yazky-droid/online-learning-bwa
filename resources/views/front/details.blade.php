5747d9d6c4810dbd0246063c5b0bee7450b2ac29
Yazky Maulana <yazki.maulana@gmail.com>
2025-08-02 21:53:19 +0700

    fix navbar

--- resources/views/front/details.blade.php
+++ resources/views/front/details.blade.php
@@ -21,10 +21,10 @@
             </a>
             <ul class="flex items-center gap-[30px] text-white">
                 <li>
-                    <a href="" class="font-semibold">Home</a>
+                    <a href="{{ route('front.index') }}" class="font-semibold">Home</a>
                 </li>
                 <li>
-                    <a href="pricing.html" class="font-semibold">Pricing</a>
+                    <a href="{{ route('front.pricing') }}" class="font-semibold">Pricing</a>
                 </li>
                 <li>
                     <a href="" class="font-semibold">Benefits</a>
@@ -33,15 +33,30 @@
                     <a href="" class="font-semibold">Stories</a>
                 </li>
             </ul>
+
+            @auth
             <div class="flex gap-[10px] items-center">
                 <div class="flex flex-col items-end justify-center">
-                    <p class="font-semibold text-white">Hi, Annasia</p>
-                    <p class="p-[2px_10px] rounded-full bg-[#FF6129] font-semibold text-xs text-white text-center">PRO</p>
+                    <p class="font-semibold text-white">Hi, {{ Auth::user()->name }}</p>
+                    @if(Auth::user()->hasActiveSubscription())
+                        <p class="p-[2px_10px] rounded-full bg-[#FF6129] font-semibold text-xs text-white text-center">PRO</p>
+                    @endif
                 </div>
                 <div class="w-[56px] h-[56px] overflow-hidden rounded-full flex shrink-0">
-                    <img src="{{ asset('assets/photo/photo5.png') }}" class="w-full h-full object-cover" alt="photo">
+                    <a href="{{ route('dashboard') }}">
+                        <img src="{{ asset('assets/photo/photo5.png') }}" class="w-full h-full object-cover" alt="photo">
+                    </a>
                 </div>
             </div>
+            @endauth
+            
+            @guest
+            <div class="flex gap-[10px] items-center">
+                <a href="{{ route('register') }}" class="text-white font-semibold rounded-[30px] p-[16px_32px] ring-1 ring-white transition-all duration-300 hover:ring-2 hover:ring-[#FF6129]">Sign Up</a>
+                <a href="{{ route('login') }}" class="text-white font-semibold rounded-[30px] p-[16px_32px] bg-[#FF6129] transition-all duration-300 hover:shadow-[0_10px_20px_0_#FF612980]">Sign In</a>
+            </div>
+            @endguest
+
         </nav>
     </div>
     <section id="video-content" class="max-w-[1100px] w-full mx-auto mt-[130px]">
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -16,10 +16,10 @@
             </a>
             <ul class="flex items-center gap-[30px] text-white">
                 <li>
-                    <a href="" class="font-semibold">Home</a>
+                    <a href="{{ route('front.index') }}" class="font-semibold">Home</a>
                 </li>
                 <li>
-                    <a href="pricing.html" class="font-semibold">Pricing</a>
+                    <a href="{{ route('front.pricing') }}" class="font-semibold">Pricing</a>
                 </li>
                 <li>
                     <a href="" class="font-semibold">Benefits</a>
@@ -28,10 +28,30 @@
                     <a href="" class="font-semibold">Stories</a>
                 </li>
             </ul>
+
+            @auth
+            <div class="flex gap-[10px] items-center">
+                <div class="flex flex-col items-end justify-center">
+                    <p class="font-semibold text-white">Hi, {{ Auth::user()->name }}</p>
+                    @if(Auth::user()->hasActiveSubscription())
+                        <p class="p-[2px_10px] rounded-full bg-[#FF6129] font-semibold text-xs text-white text-center">PRO</p>
+                    @endif
+                </div>
+                <div class="w-[56px] h-[56px] overflow-hidden rounded-full flex shrink-0">
+                    <a href="{{ route('dashboard') }}">
+                        <img src="{{ asset('assets/photo/photo5.png') }}" class="w-full h-full object-cover" alt="photo">
+                    </a>
+                </div>
+            </div>
+            @endauth
+            
+            @guest
             <div class="flex gap-[10px] items-center">
                 <a href="{{ route('register') }}" class="text-white font-semibold rounded-[30px] p-[16px_32px] ring-1 ring-white transition-all duration-300 hover:ring-2 hover:ring-[#FF6129]">Sign Up</a>
                 <a href="{{ route('login') }}" class="text-white font-semibold rounded-[30px] p-[16px_32px] bg-[#FF6129] transition-all duration-300 hover:shadow-[0_10px_20px_0_#FF612980]">Sign In</a>
             </div>
+            @endguest
+
         </nav>
         <div class="flex flex-col items-center gap-[30px]">
             <div class="w-fit flex items-center gap-3 p-2 pr-6 rounded-full bg-[#FFFFFF1F] border border-[#3477FF24]">
