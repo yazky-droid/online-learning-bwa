383765aadc67d3e88c31150f087d72c3d3f62994
Yazky Maulana <yazki.maulana@gmail.com>
2025-08-02 21:26:57 +0700

    add feature learning

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FrontController extends Controller
 {
@@ -17,5 +18,20 @@ class FrontController extends Controller
     public function details(Course $course){
 
         return view('front.details', compact('course'));
+    }
+
+    public function learning(Course $course, $courseVideoId){
+
+        $user = Auth::user();
+
+        if(!$user->hasActiveSubscription()){
+            return redirect()->route('front.pricing');
+        }
+
+        $video = $course->course_videos()->firstWhere('id', $courseVideoId);
+
+        $user->courses()->syncWithoutDetaching($course->id);
+
+        return view('front.learning', compact('course', 'video'));
     }
 }
--- resources/views/front/learning.blade.php
+++ resources/views/front/learning.blade.php
@@ -48,7 +48,7 @@
         <div class="video-player relative flex flex-nowrap gap-5">
             <div class="plyr__video-embed w-full overflow-hidden relative rounded-[20px]" id="player">
                 <iframe
-                    src="https://www.youtube.com/embed/{{ $course->path_trailer }}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
+                    src="https://www.youtube.com/embed/{{ $video->path_video }}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
                     allowfullscreen
                     allowtransparency
                     allow="autoplay"
@@ -57,26 +57,31 @@
             <div class="video-player-sidebar flex flex-col shrink-0 w-[330px] h-[470px] bg-[#F5F8FA] rounded-[20px] p-5 gap-5 pb-0 overflow-y-scroll no-scrollbar">
                 <p class="font-bold text-lg text-black">{{ $course->course_videos->count() }} Lessons</p>
                 <div class="flex flex-col gap-3">
-                    <div class="group p-[12px_16px] flex items-center gap-[10px] bg-[#3525B3] rounded-full hover:bg-[#3525B3] transition-all duration-300">
-                        <div class="text-white group-hover:text-white transition-all duration-300">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M11.97 2C6.44997 2 1.96997 6.48 1.96997 12C1.96997 17.52 6.44997 22 11.97 22C17.49 22 21.97 17.52 21.97 12C21.97 6.48 17.5 2 11.97 2ZM14.97 14.23L12.07 15.9C11.71 16.11 11.31 16.21 10.92 16.21C10.52 16.21 10.13 16.11 9.76997 15.9C9.04997 15.48 8.61997 14.74 8.61997 13.9V10.55C8.61997 9.72 9.04997 8.97 9.76997 8.55C10.49 8.13 11.35 8.13 12.08 8.55L14.98 10.22C15.7 10.64 16.13 11.38 16.13 12.22C16.13 13.06 15.7 13.81 14.97 14.23Z" fill="currentColor"/>
-                            </svg>
-                        </div>
-                        <a href="{{ route('front.details', $course) }}">
-                            <p class="font-semibold group-hover:text-white transition-all duration-300 text-white">Course Trailer</p>
-                        </a>
-                    </div>
-
-                    @forelse ($course->course_videos as $video)
-                    <div class="group p-[12px_16px] flex items-center gap-[10px] bg-[#E9EFF3]  rounded-full hover:bg-[#3525B3] transition-all duration-300">
+                    <div class="group p-[12px_16px] flex items-center gap-[10px] bg-[#E9EFF3] rounded-full hover:bg-[#3525B3] transition-all duration-300">
                         <div class="text-black group-hover:text-white transition-all duration-300">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M11.97 2C6.44997 2 1.96997 6.48 1.96997 12C1.96997 17.52 6.44997 22 11.97 22C17.49 22 21.97 17.52 21.97 12C21.97 6.48 17.5 2 11.97 2ZM14.97 14.23L12.07 15.9C11.71 16.11 11.31 16.21 10.92 16.21C10.52 16.21 10.13 16.11 9.76997 15.9C9.04997 15.48 8.61997 14.74 8.61997 13.9V10.55C8.61997 9.72 9.04997 8.97 9.76997 8.55C10.49 8.13 11.35 8.13 12.08 8.55L14.98 10.22C15.7 10.64 16.13 11.38 16.13 12.22C16.13 13.06 15.7 13.81 14.97 14.23Z" fill="currentColor"/>
                             </svg>
                         </div>
+                        <a href="{{ route('front.details', $course) }}">
+                            <p class="font-semibold group-hover:text-white transition-all duration-300">Course Trailer</p>
+                        </a>
+                    </div>
+
+                    @forelse ($course->course_videos as $video)
+
+                    @php
+                        $currentVideoId = Route::current()->parameter('courseVideoId');
+                        $isActive = $currentVideoId == $video->id;
+                    @endphp
+                    <div class="group p-[12px_16px] flex items-center gap-[10px] {{ $isActive ? 'bg-[#3525B3]' : 'bg-[#E9EFF3]' }}  rounded-full hover:bg-[#3525B3] transition-all duration-300">
+                        <div class="{{ $isActive ? 'text-white' : 'text-black' }} group-hover:text-white transition-all duration-300">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11.97 2C6.44997 2 1.96997 6.48 1.96997 12C1.96997 17.52 6.44997 22 11.97 22C17.49 22 21.97 17.52 21.97 12C21.97 6.48 17.5 2 11.97 2ZM14.97 14.23L12.07 15.9C11.71 16.11 11.31 16.21 10.92 16.21C10.52 16.21 10.13 16.11 9.76997 15.9C9.04997 15.48 8.61997 14.74 8.61997 13.9V10.55C8.61997 9.72 9.04997 8.97 9.76997 8.55C10.49 8.13 11.35 8.13 12.08 8.55L14.98 10.22C15.7 10.64 16.13 11.38 16.13 12.22C16.13 13.06 15.7 13.81 14.97 14.23Z" fill="currentColor"/>
+                            </svg>
+                        </div>
                         <a href="{{ route('front.learning', [$course, 'courseVideoId' => $video->id]) }}">
-                            <p class="font-semibold group-hover:text-white transition-all duration-300">{{ $video->name }}</p>
+                            <p class="font-semibold group-hover:text-white transition-all duration-300 {{ $isActive ? 'text-white' : '' }}">{{ $video->name }}</p>
                         </a>
                     </div>
                     @empty
